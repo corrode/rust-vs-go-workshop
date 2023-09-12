de0e2e2a807b7dd49689d21deaa8bb4985bf8eb7
Matthias <matthias-endler@gmx.net>
2023-09-12 16:17:22 +0200

    savepoint

---
+++ rust/rust-3/src/main.rs
@@ -0,0 +1,152 @@
+use anyhow::Context;
+use askama::Template;
+use axum::{
+    extract::Query,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
+use std::net::SocketAddr;
+
+// Make our own error that wraps `anyhow::Error`.
+struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+// This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
+// `Result<_, AppError>`. That way you don't need to do that manually.
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
+
+#[derive(Deserialize, Debug)]
+pub struct GeoResponse {
+    pub results: Vec<LatLong>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct LatLong {
+    pub latitude: f64,
+    pub longitude: f64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct WeatherResponse {
+    pub latitude: f64,
+    pub longitude: f64,
+    pub timezone: String,
+    pub hourly: Hourly,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Hourly {
+    pub time: Vec<String>,
+    pub temperature_2m: Vec<f64>,
+}
+
+#[derive(Template)]
+#[template(path = "weather.html")]
+#[derive(Deserialize, Debug)]
+pub struct WeatherDisplay {
+    pub city: String,
+    pub forecasts: Vec<Forecast>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Forecast {
+    pub date: String,
+    pub temperature: String,
+}
+
+async fn fetch_lat_long(city: &str) -> Result<LatLong, anyhow::Error> {
+    let endpoint = format!(
+        "https://geocoding-api.open-meteo.com/v1/search?name={}&count=1&language=en&format=json",
+        city
+    );
+    let response = reqwest::get(&endpoint).await?.json::<GeoResponse>().await?;
+    response.results.get(0).cloned().context("No results found")
+}
+
+async fn fetch_weather(lat_long: LatLong) -> Result<WeatherResponse, anyhow::Error> {
+    let endpoint = format!(
+        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&hourly=temperature_2m",
+        lat_long.latitude, lat_long.longitude
+    );
+    let response = reqwest::get(&endpoint)
+        .await?
+        .json::<WeatherResponse>()
+        .await?;
+    Ok(response)
+}
+
+// basic handler that responds with a static string
+async fn index() -> &'static str {
+    "Index"
+}
+
+#[derive(Deserialize)]
+pub struct WeatherQuery {
+    pub city: String,
+}
+
+impl TryFrom<WeatherResponse> for WeatherDisplay {
+    type Error = anyhow::Error;
+
+    fn try_from(response: WeatherResponse) -> Result<Self, Self::Error> {
+        let display = WeatherDisplay {
+            city: response.timezone,
+            forecasts: response
+                .hourly
+                .time
+                .iter()
+                .zip(response.hourly.temperature_2m.iter())
+                .map(|(date, temperature)| Forecast {
+                    date: date.to_string(),
+                    temperature: temperature.to_string(),
+                })
+                .collect(),
+        };
+        Ok(display)
+    }
+}
+
+async fn weather(Query(params): Query<WeatherQuery>) -> Result<WeatherDisplay, AppError> {
+    let lat_long = fetch_lat_long(&params.city).await?;
+    let weather = fetch_weather(lat_long).await?;
+    let display = WeatherDisplay::try_from(weather)?;
+    Ok(display)
+}
+
+async fn stats() -> &'static str {
+    "Stats"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/weather", get(weather))
+        .route("/stats", get(stats));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
